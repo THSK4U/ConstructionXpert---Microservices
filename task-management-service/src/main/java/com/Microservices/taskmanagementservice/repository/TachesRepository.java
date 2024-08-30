9d6847102f4486658188e1af1c284e16abc484f1
TAHA SELLAK <sellaktaha1@gmail.com>
2024-08-30 12:06:39 +0100

    Ajout méthode findAllByProjetId sut TachesRepository

--- task-management-service/src/main/java/com/Microservices/taskmanagementservice/repository/TachesRepository.java
+++ task-management-service/src/main/java/com/Microservices/taskmanagementservice/repository/TachesRepository.java
@@ -3,5 +3,9 @@ package com.Microservices.taskmanagementservice.repository;
 import com.Microservices.taskmanagementservice.model.Entity.Taches;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TachesRepository extends JpaRepository<Taches, Long> {
+
+    List<Taches> findAllByProjetId(Long id);
 }
