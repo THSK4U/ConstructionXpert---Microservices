8383c2b4d43d8a218ae26893017ff6c6b30fe5aa
TAHA SELLAK <sellaktaha1@gmail.com>
2024-09-04 16:54:59 +0100

    Ajouter Controller EndPoint Validate Token

--- GestionUtilisateurs-service/src/main/java/com/Microservices/GestionUtilisateurs_service/Controller/AuthenticationController.java
+++ GestionUtilisateurs-service/src/main/java/com/Microservices/GestionUtilisateurs_service/Controller/AuthenticationController.java
@@ -3,17 +3,18 @@ package com.Microservices.GestionUtilisateurs_service.Controller;
 import com.Microservices.GestionUtilisateurs_service.Model.Dto.UserDTO;
 import com.Microservices.GestionUtilisateurs_service.Model.Entity.AuthResponse;
 import com.Microservices.GestionUtilisateurs_service.Service.AuthService;
+import com.Microservices.GestionUtilisateurs_service.Service.securitySevice.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AuthenticationController {
     @Autowired
     private AuthService Service;
+
+    @Autowired
+    private JwtService JWTService;
 
 @PostMapping("/Auth/register")
     public ResponseEntity<AuthResponse> register(
@@ -25,4 +26,11 @@ public class AuthenticationController {
             @RequestBody UserDTO user){
         return ResponseEntity.ok(Service.authenticate(user));
     }
+
+    @GetMapping("/Auth/validate")
+    public ResponseEntity<String> validateToken(@RequestParam("token") String token) {
+        JWTService.validateToken(token);
+        return ResponseEntity.ok("Token is valid");
+    }
+
 }
